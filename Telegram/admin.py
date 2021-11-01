bda01be90bbe8e7cd226be941130c99d499034f8
emoji3090 <emoji3090@gmail.com>
2021-11-01 21:08:17 +0330

    v 0

--- Telegram/admin.py
+++ Telegram/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+
+    def get_queryset(self, request):
+        qs = super(ProfileAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user=request.user)
+
+
+    list_display = ('id', 'user')
+    search_fields = ('id', 'user')
+
--- Telegram/views.py
+++ Telegram/views.py
@@ -1,17 +1,24 @@
 import time
-
+from Telegram.models import *
 from django.http import HttpResponse
 from telegram.ext import Updater
 import telegram
+from datetime import date
 
 
 def index(request):
-    token = '2075266445:AAEL05k7urfQ4uGV3Rt-KxuTlI7HxDLc1yc'
-    bot = telegram.Bot(token=token)
-    chat_id = -1001326811432
-    user_id = 1687107831
-    bot.kick_chat_member(
-        chat_id=chat_id,
-        user_id=user_id,)
-    response = HttpResponse('Done')
+    # token = '2075266445:AAEL05k7urfQ4uGV3Rt-KxuTlI7HxDLc1yc'
+    # bot = telegram.Bot(token=token)
+    # today = date.today()
+    #
+    # chat_id = -1001326811432
+    # user_id = 1687107831
+    #
+    # members = Member.objects.filter(date=today)
+    # for item in members
+    # a = bot.getChatMember(chat_id=chat_id)
+    # bot.kick_chat_member(
+    #     chat_id=chat_id,
+    #     user_id=user_id)
+    response = HttpResponse("a")
     return response
