5922f7139827bb78a52ba97b332a1b368ee7585e
emoji3090 <emoji3090@gmail.com>
2021-11-01 13:10:05 +0330

    V 0

--- Telegram/views.py
+++ Telegram/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
+from telegram.ext import Updater
+import telegram
 
 
 def index(request):
+    token = '2075266445:AAEL05k7urfQ4uGV3Rt-KxuTlI7HxDLc1yc'
+    bot = telegram.Bot(token=token)
+    chat_id = 1539258067
+    bot.sendMessage(chat_id=chat_id, text='bot_welcome')
     response = HttpResponse('Done')
     return response
